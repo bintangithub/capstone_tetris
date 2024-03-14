3d08bf53317a5c4106ff5a41c7bd2cb0819dad01
bintangithub <121070479+gulangemilang@users.noreply.github.com>
2024-03-15 00:34:53 +0700

    rombak pertama

--- dashboard.py
+++ dashboard.py
@@ -2,56 +2,113 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import numpy as np
-from scipy.spatial import distance
 
 hitung_jarak=pd.read_excel(r"data_pengukuran_jarak(2).xlsx",index_col=0)
 visualisasi_data=pd.read_excel(r"data_visualisasi(4).xlsx",index_col=0)
+ranktrix=pd.read_csv(r"rank_matrix.csv",index_col=0)
+halase=pd.read_csv(r"od_17052_harapan_lama_sekolah_berdasarkan_kabupatenkota (1)\data.jabarprov.go.id\bps-od_17052_harapan_lama_sekolah_berdasarkan_kabupatenkota_data.csv",index_col=0)
+wifi=pd.read_csv(r"od_17306_jml_titik_layanan_akses_internet_publik_wifi__kabupate\data.jabarprov.go.id\diskominfo-od_17306_jml_titik_layanan_akses_internet_publik_wifi__kabupate_data.csv",index_col=0)
+uhahi=pd.read_csv(r"od_15038_usia_harapan_hidup_berdasarkan_kabupatenkota\data.jabarprov.go.id\bps-od_15038_usia_harapan_hidup_berdasarkan_kabupatenkota_data.csv",index_col=0)
+kemiskinan=pd.read_csv(r"od_19998_indeks_keparahan_kemiskinan_berdasarkan_kabupatenkota\data.jabarprov.go.id\bps-od_19998_indeks_keparahan_kemiskinan_berdasarkan_kabupatenkota_data.csv",index_col=0)
+database=pd.read_csv("_select_hls_nama_kabupaten_kota_as_kabkot_hls_harapan_lama_sekol_202402070903.csv",index_col=0)
 
+# =============================================================================
+# KUMPULAN DARI FUNGSI UNTUK KEBUTUHAN TAMPILAN DAN LAIN LAIN
+# =============================================================================
 
-#UPDATE LABEL GRAPH
-def update_graph(tahun_terpilih):
-   dff=visualisasi_data[(visualisasi_data["Tahun"]>=tahun_terpilih[0])&(visualisasi_data["Tahun"]<=tahun_terpilih[1])]
-   plot=px.scatter(
-   data_frame=dff,
-   x="xaxis",
-   y="yaxis",
-   color="Label",
-   hover_data=["Tahun"],
-   hover_name="Kabkot",
-   labels="Tahun",
-   marginal_x="histogram"
-   )
-   plot.update_layout(coloraxis_showscale=False)
-    
-   return (plot)
+def ketimpangan_kab(kabupaten,tahun):
+    selected=[]
+    if kabupaten==[]:
+        kabupaten=["KABUPATEN BANDUNG"]
+    for kab in kabupaten:
+        index=ranktrix[(ranktrix.tahun==tahun)&(ranktrix.kabkot==kab)].index[0]
+        df=pd.DataFrame(ranktrix[ranktrix.tahun==tahun].loc[index]).T
+        selected.append(df)
+    df=pd.concat(selected)
+    df.rename(columns={"sum":"Nilai Wilayah","kabkot":"Kabupaten/Kota"},inplace=True)
+    plot=px.bar(df,x="Kabupaten/Kota",y="Nilai Wilayah",text="Nilai Wilayah")
+    plot.update_traces(textposition='inside')
+    plot.update_layout(yaxis={'visible': False, 'showticklabels': False})
+    return plot,df
 
-# UPDATE KETIMPANGAN GRAPH
-def perhitungan(kota_acuan,kota_terhitung,tahun):
-    if kota_terhitung==[]:
-        kota_terhitung=["KABUPATEN BANDUNG"]
-    full=[]
-    jarak=[]
-    for kabkot in kota_terhitung:
-        full.append(hitung_jarak[(hitung_jarak.kabkot==kabkot)&(hitung_jarak.tahun==tahun)])
-    full=pd.concat(full).reset_index(drop=True).set_index("kabkot")
-    for i in range(len(full)):
-        kota1=np.squeeze(np.array(full.loc[full.index[i],"halase":"uhahi"]))
-        kota2=np.squeeze(np.array(hitung_jarak[(hitung_jarak.kabkot==kota_acuan)&(hitung_jarak.tahun==tahun)].loc[:,"halase":"uhahi"]))
-        jrk=distance.euclidean(kota1,kota2)
-        jarak.append(jrk)
-    jarak=pd.DataFrame(jarak,columns=[kota_acuan],index=full.index)
-    jarak=pd.concat([jarak,full.label],axis=1)
-    label=np.array(hitung_jarak[(hitung_jarak.kabkot==kota_acuan)&(hitung_jarak.tahun==tahun)].loc[:,"label"])[0]
-    plot=px.bar(
-        jarak
-        ,color=jarak.index
-        ,title=f"Perbandingan Jarak Ketimpangan dengan {kota_acuan} ({label})"
-        ,hover_data=["label"]
-    )
-    # plot.update_traces(width = 0.5)
-    return (plot)
+def show_tabel(df):
+    if df=="Harapan Lama Sekolah":
+        st.dataframe(halase)
+    elif df=="Banyak Titik WiFi":
+        st.dataframe(wifi)
+    elif df=="Usia Harapan Hidup":
+        st.dataframe(uhahi)
+    else:
+        st.dataframe(kemiskinan)
 
-
+def show_hist(aspek):
+    col1,col2=st.columns(2)
+    if aspek=="Harapan Lama Sekolah":
+        hist=px.histogram(database.halase)
+        st.plotly_chart(hist)
+        col2.info("""
+                Range dari Harapan Lama Sekolah:
+                
+                Range 1: 11.69 - 12.1275
+                
+                Range 2: 12.1276 - 12.565
+                
+                Range 3: 12.566 - 13.4275
+                
+                Range 4: 13.4276 - 14.29
+                """)
+        
+    elif aspek=="Banyak Titik WiFi":
+        hist=px.histogram(database.wifi)
+        st.plotly_chart(hist)
+        col2.info("""
+                Range dari WiFi:
+                
+                Range 1: 0 - 2.5
+                
+                Range 2: 2.6 - 5.0
+                
+                Range 3: 5.1 - 11.5
+                
+                Range 4: 11.6 - 18.0
+                """)
+    elif aspek=="Usia Harapan Hidup":
+        hist=px.histogram(database.uhahi)
+        st.plotly_chart(hist)
+        col2.info("""
+                Range dari Usia Harapan Hidup:
+                
+                Range 1: 69.21 - 70.85
+                
+                Range 2: 70.86 - 72.49
+                
+                Range 3: 72.50 - 74.14
+                
+                Range 4: 74.15 - 75.79
+                """)
+    else:
+        hist=px.histogram(database.kemiskinan)
+        st.plotly_chart(hist)
+        col2.info("""
+                Range dari Kemiskinan:
+                
+                Range 1: 0.03 - 0.145
+                
+                Range 2: 0.146 - 0.26
+                
+                Range 3: 0.27 - 0.515
+                
+                Range 4: 0.516 - 0.77
+                """)
+    col1.markdown("""
+                  <div style="text-align: justify;">
+                  Analisis deskriptif melalui histogram menghasilkan range/rentang dari data setiap tabel
+                  yang masing-masing rentang tersebut akan dinilai bobotnya sesuai dengan karakteristik 
+                  data
+                  
+                  </div>
+                  """,unsafe_allow_html=True)
+                  
 # =============================================================================
 # CONTAINER UNTUK TITLE HINGGA ABSTRAK
 # =============================================================================
@@ -68,86 +125,126 @@ with st.container():
             <div style="text-align: justify;">Jawa Barat merupakan salah satu provinsi di Indonesia yang terletak di sebelah barat pulau Jawa. Provinsi tersebut terletak bersebelahan dengan ibu kota negara yaitu Jakarta. 
             Lokasi geografis dari provinsi yang dekat dengan ibu kota menimbulkan pertanyaan apakah terdapat ketimpangan wilayah yang terjadi di provinsi tersebut karena ketimpangan dapat 
             terjadi dalam skala nasional atau pun lokal. Penelitian ini menilai ketimpangan wilayah dari 4 aspek antara lain Infrastruktur (Banyak Titik WiFi), Kependudukan (Usia Harapan Hidup), 
-            Kemiskinan (Indeks Keparahan Kemiskinan), dan Pendidikan (Harapan Lama Sekolah). Ketimpangan tersebut dinlai melalui analisis AHP yang diklaster melalui K-Means. 
-            Label yang didapatkan dari klasterisasi terdapat 8 jenis ketimpangan.</div>
+            Kemiskinan (Indeks Keparahan Kemiskinan), dan Pendidikan (Harapan Lama Sekolah). Ketimpangan tersebut dinilai melalui analisis AHP</div>
             
             """,unsafe_allow_html=True)
-
-# =============================================================================
-# CONTAINER UNTUK PEMBAHASAN AHP
-# =============================================================================
-
-with st.container():
-   st.header("Analisis Aspek Prioritas")
-   st.markdown("""
-            <div style="text-align: justify;">
-            Analisis AHP dilakukan pada setiap aspek yang dianalisis antara lain Infrastruktur, Kependudukan, Kemiskinan, dan Pendidikan. Berdasarkan beberapa jurnal yang dibaca 
-            dapat disimpulkan bahwa urutan aspek prioritas antara lain:
             
-            1. Usia Harapan Hidup (Kependudukan)
-            2. Indeks Keparahan Kemiskinan (Kemiskinan)
-            3. Harapan Lama Sekolah (Pendidikan)
-            4. Banyak Titik WiFi (Infrastruktur)
+# =============================================================================
+# CONTAINER UNTUK PENJELASAN LENGKAP ANALISIS AHP
+# =============================================================================
+with st.container():
+    st.header("Analisis Analytical Hierarchy Process (AHP)")
+    st.markdown("""
+            <div style="text-align: justify;">
+            Analisis AHP merupakan metode pengambilan keputusan yang membandingkan alternatif dari pilihan. Metode ini diterapkan untuk analisis aspek apa saja yang merupakan prioritas penyebab ketimpangan wilayah kabupaten di Jawa Barat. 
+            Pada analisis ketimpangan kabupaten di Jawa Barat dilakukan terhadap aspek antara lain Infrastruktur (Banyak Titik WiFi), Kependudukan (Usia Harapan Hidup), Kemiskinan (Indeks Keparahan Kemiskinan), 
+            dan Pendidikan (Harapan Lama Sekolah). Analisis ini dapat disimak melalui tahapan seperti  di bawah.
             
             </div>
             
             """,unsafe_allow_html=True)
-
-# =============================================================================
-# CONTAINER UNTUK GRAPH LABEL
-# =============================================================================
-with st.container():
-    st.header("Persebaran Label Ketimpangan Wilayah Kabupaten di Jawa Barat")
+    st.subheader("Pengumpulan Tabel")
+    st.markdown("""
+            <div style="text-align: justify;">
+            Pengumpulan tabel dilakukan untuk mendapatkan nilai-nilai dari setiap aspek yang dianalisis. Tabel tersebut didapatkan dari Open Data Jabar secara online. Pengumpulan tabel dilakukan juga dimaksud untuk mendapatkan insight
+            dari berbagai nilai yang dianalisis melalui AHP. Tabel-tabel yang dikumpulkan dapat diakses melalui link di bawah.
+            
+            1. [Banyak Titik Wifi](https://opendata.jabarprov.go.id/id/dataset/jumlah-titik-layanan-akses-internet-publik-wifi-berdasarkan-kabupatenkota-di-jawa-barat)
+            2. [Usia Harapan Hidup](https://opendata.jabarprov.go.id/id/dataset/usia-harapan-hidup-berdasarkan-kabupatenkota-di-jawa-barat)
+            3. [Kemiskinan](https://opendata.jabarprov.go.id/id/dataset/indeks-keparahan-kemiskinan-berdasarkan-kabupatenkota-di-jawa-barat)
+            4. [Harapan Lama Sekolah](https://opendata.jabarprov.go.id/id/dataset/harapan-lama-sekolah-berdasarkan-kabupatenkota-di-jawa-barat)
+            
+            
+            </div>
+            
+            """,unsafe_allow_html=True)
+    st.markdown("""
+            <div style="text-align: center;">
+            TAMPILKAN TABEL YANG ANDA MAU !!
+            </div>
+            """,unsafe_allow_html=True)
+    df=st.selectbox("Pilih Tabel",["Harapan Lama Sekolah","Banyak Titik WiFi","Usia Harapan Hidup","Tingkat Keparahan Kemiskinan"],key=0)
+    show_tabel(df)
+    st.subheader("Analisis Deskriptif Tabel")
+    st.markdown("""
+            <div style="text-align: justify;">
+            Analisis deskriptif merupakan sebuah tahap yang dilakukan untuk menentukan range dari aspek melalui penilaian verbal pada analisis AHP. Analisis deskriptif dapat dilakukan
+            melalui menampilkan persebaran data dari sebuah tabel yang berisi nilai dari kumpulan tabel di atas dari tahun 2019 hingga tahun 2023.
+            
+            </div>
+            """,unsafe_allow_html=True)
+    st.markdown("""
+            <div style="text-align: center;"><br/>
+            TAMPILKAN DISTRIBUSI TABEL YANG ANDA MAU !!</br>
+            </div>
+            """,unsafe_allow_html=True)
+    aspek=st.selectbox("Pilih Tabel",["Harapan Lama Sekolah","Banyak Titik WiFi","Usia Harapan Hidup","Tingkat Keparahan Kemiskinan"],key=1)
+    show_hist(aspek)
+    st.subheader("Studi Literatur")
+    st.markdown(
+            """
+            <div style="text-align: justify;">
+            Studi literatur merupakan metode yang sangat penting untuk menentukan penentuan bobot antara aspek. Studi literatur dilakukan melalui paper-paper
+            yang mengandung aspek-aspek yang diteliti serta hubungannya. Contoh: Hubungan WiFi dengan Pendidikan. Literasi yang dipilih juga akan mengandung 
+            kata kunci seperti ketimpangan wilayah, wifi, kemiskinan, pendidikan, kehidupan, dan jawa barat. Literasi atau jurnal yang dipilih dapat dilihat pada 
+            bagian referensi.
+            </div>
+            """,unsafe_allow_html=True)
+    st.subheader("Menentukan Beban per Kabupaten")
     st.markdown("""
                 <div style="text-align: justify;">
-                Label ketimpangan yang didapatkan diurutkan berdasarkan berat matriks verbal judgment
-                dan dijadikan ordinal. Label tersebut antara lain:
-                    
-                1. Sempurna
-                2. Luar Biasa
-                3. Sangat Baik
-                4. Baik
-                5. Cukup Baik
-                6. Kurang Baik
-                7. Buruk
-                8. Sangat Buruk</div>
+                Terakhir untuk menentukan ketimpangan dari masing-masing kabupaten di Jawa Barat. Beban dari masing-masing kabupaten berdasarkan analisis verbal range/rentang dan analisis verbal aspek. 
+                Dua nilai analisis tersebut akan dinormalisasi sehingga didapatkan nilai aspek prioritas dari masing-masing analisis. Kemudian nilai ketimpangan akan didapatkan ketika jumlah dari hasil kali analisis
+                aspek prioritas dan range/rentang didapatkan. 
+                
                 """,unsafe_allow_html=True)
-    value=st.slider("Pilih Rentang Tahun",min_value=2019,max_value=2023,value=[2019,2023],label_visibility="visible")
-    plot=update_graph(value)
-    st.plotly_chart(plot)
+    st.markdown("""<b/>Analisis dapat dilihat pada file excel berikut :
+                [Analisis AHP](https://github.com/bintangithub/capstone_tetris/blob/46a1ced457e86773846e7a28d37e001464e5d292/Analisis%20AHP%20by%20excel.xlsx)</b>""",unsafe_allow_html=True)
+
+
+# =============================================================================
+# CONTAINER UNTUK ANALISIS PERSEBARAN KETIMPANGAN
+# =============================================================================
+with st.container():
+    st.header("Persebaran Ketimpangan Wilayah per Tahun")
+    std=[]
+    for i in range(2019,2024):
+        std.append(ranktrix[ranktrix.tahun==i].loc[:,"sum"].std())
+    persebaran_ketimpangan=pd.DataFrame({"Tahun":ranktrix.sort_values("tahun")["tahun"].unique(),"Persebaran Ketimpangan":std})
+    ketimpangan=px.line(persebaran_ketimpangan,x="Tahun",y="Persebaran Ketimpangan")
+    ketimpangan.update_xaxes(tickvals=persebaran_ketimpangan['Tahun'], ticktext=persebaran_ketimpangan['Tahun'].astype(str))
+    st.plotly_chart(ketimpangan)
+
+# =============================================================================
+# CONTAINER UNTUK ANALISIS KETIMPANGAN PER KABUPATEN PER TAHUN
+# =============================================================================
+with st.container():
+    st.header("Perbandingan Ketimpangan Wilayah per Kabupaten per Tahun")
+    tahun=st.radio("Pilih Tahun",ranktrix.sort_values("tahun")["tahun"].unique(),horizontal=True)
+    kotkab=st.multiselect("Pilih Kabupaten",ranktrix.sort_values("kabkot")["kabkot"].unique(),placeholder="Kabupaten/Kota",default=["KABUPATEN BANDUNG","KOTA BANDUNG","KABUPATEN TASIKMALAYA"])
+    ketimpangankab,df=ketimpangan_kab(kotkab, tahun)
+    s=""
+    if len(df)==1:
+        s=df[df.loc[:,"Nilai Wilayah"]==df.loc[:,"Nilai Wilayah"].max()]["Kabupaten/Kota"].values[0]
+    else:
+        for i in df[df.loc[:,"Nilai Wilayah"]==df.loc[:,"Nilai Wilayah"].max()]["Kabupaten/Kota"].values:
+            if np.where(df[df.loc[:,"Nilai Wilayah"]==df.loc[:,"Nilai Wilayah"].max()]["Kabupaten/Kota"].values==i)[0][0]==(len(df[df.loc[:,"Nilai Wilayah"]==df.loc[:,"Nilai Wilayah"].max()]["Kabupaten/Kota"].values)-1):
+                s+=" " + i + "."
+            else:
+                s+=" " + i + ","
+    st.info(
+        f"""
+        **TERBAIK**
+        
+        KABUPATEN/KOTA : {s}
+        
+        Nilai Wilayah : {df.loc[:,"Nilai Wilayah"].max()}
+        """
+        
+        )
+    st.plotly_chart(ketimpangankab)
     
     
-# =============================================================================
-# CONTAINER UNTUK BAR CHART (JARAK PER KABUPATEN SETIAP TAHUN)
-# =============================================================================
-with st.container(): 
-   st.header("Jarak Ketimpangan Wilayah per Kabupaten/Kota setiap Tahun")
-   st.markdown("""
-            <div style="text-align: justify;">Jarak ketimpangan merupakan pengukuran seberapa jauh atau dekat suatu kabupaten terukur 
-            terhadap kabupaten acuan. Semakin dekat kabupaten terukur dengan kabupaten acuan maka jarak akan semakin kecil dan 
-            semakin tidak timpang. Jarak ketimpangan dapat dilihat pada diagram blok di bawah.
-            </div>
-            """,unsafe_allow_html=True)
-   tahun=st.radio("Pilih Tahun",hitung_jarak.sort_values("tahun")["tahun"].unique(),horizontal=True)
-   kota_acuan=st.selectbox("Pilih Kabupaten Acuan",hitung_jarak.sort_values("kabkot")["kabkot"].unique(),placeholder="Kabupaten/Kota")
-   kota_terhitung=st.multiselect("Pilih Kabupaten Terukur",hitung_jarak.sort_values("kabkot")["kabkot"].unique(),placeholder="Kabupaten/Kota")
-   barchart=perhitungan(kota_acuan, kota_terhitung, tahun)
-   st.plotly_chart(barchart)
-   
-# =============================================================================
-# CONTAINER UNTUK CONCLUSION
-# =============================================================================
-with st.container(): 
-   st.header("Kesimpulan")
-   st.markdown("""
-            <div style="text-align: justify;">
-            
-            1. Terdapat ketimpangan wilayah yang besar terjadi antara Kabupaten  berlabel sempurna dengan Kabupaten berlabel Kurang Baik, Buruk, dan Sangat Buruk.
-            2. Pada tahun 2023 terdapat 5 Kabupaten/Kota yang sudah termasuk dalam label sempurna sedangkan masih sebanyak 5 yang termasuk Sangat Buruk, Buruk, atau Kurang Baik.
-            3. Penurunan jarak ketimpangan dapat dilakukan dengan mempertimbangkan aspek prioritas. 
-            </div>
-            """,unsafe_allow_html=True)
-   
 # =============================================================================
 # CONTAINER UNTUK REFERENCE
 # =============================================================================
@@ -164,26 +261,3 @@ with st.container():
                 8. Bellmann, L., & Hübler, O. (2020). Working from home, job satisfaction and work–life balance – robust or heterogeneous links?. International Journal of Manpower. https://doi.org/10.1108/ijm-10-2019-0458.
                 9. Gragnano, A., Simbula, S., & Miglioretti, M. (2020). Work–Life Balance: Weighing the Importance of Work–Family and Work–Health Balance. International Journal of Environmental Research and Public Health, 17. https://doi.org/10.3390/ijerph17030907.
                 """)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
